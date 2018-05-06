601db1087dbe6d5c8313113cc69d16c5d395a582
adrianomundo <adriano.mundo@mail.polimi.it>
2018-05-06 02:29:37 +0200

    corrected

--- src/main/java/it/polimi/se2018/model/DiceColour.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.se2018.model;
-
-public enum DiceColor {
-    Red,
-    Yellow,
-    Blue,
-    Green,
-    Purple;
-}
--- src/main/java/it/polimi/se2018/model/PlayerColour.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.se2018.model;
-
-public enum PlayerColor {
-    Red,
-    Green,
-    Blue,
-    Purple;
-}
