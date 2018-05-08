b3afa88a6a9e2abed2629d8629728f6d7fdc91bc
adrianomundo <adriano.mundo@mail.polimi.it>
2018-05-08 15:13:08 +0200

    added get/set methods

--- src/main/java/it/polimi/se2018/model/GlassCoord.java
+++ src/main/java/it/polimi/se2018/model/GlassCoord.java
@@ -4,31 +4,26 @@ public class GlassCoord {
     private int coordX;
     private int coordY;
 
-}
 
+    public GlassCoord(int coordX, int coordY) {
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
 
-public GlassCord(int coordX, int coordY)
-{
-    this.coordX = coordX;
-    this.coordY = coordY;
-}
-public int getCoordX()
-{
-    return coordX;
-}
+    public int getCoordX() {
+        return coordX;
+    }
 
-public void setCoordX(int coordX)
-{
-    this.coordX = coordX;
-}
+    public void setCoordX(int coordX) {
+        this.coordX = coordX;
+    }
 
-public int getCoordY()
-{
-    return coordY;
-}
+    public int getCoordY() {
+        return coordY;
+    }
 
-public void setCoordY(int coordY)
-{
-    this.coordY  = coordY;
-}
+    public void setCoordY(int coordY) {
+        this.coordY = coordY;
+    }
 
+}
