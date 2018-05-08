2bb8b8766aed8853262c8b89419050b872985b7e
dariomiceli3 <dario.miceli@mail.polimi.it>
2018-05-08 23:18:56 +0200

    GSON from file

--- src/main/java/it/polimi/se2018/model/Cards/main.java
+++ src/main/java/it/polimi/se2018/model/Cards/main.java
@@ -10,18 +10,27 @@ import java.io.FileReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 
 public class main {
     public static void main(String[] args) {
 
 
-        String patterJson ="{'name': 'Virtus', 'difficulty': 4, 'pattern': [{'constraintValue': 2 ,'constraintColor': 'Blue' },{'constraintValue': 1,'constraintColor':'Green'}]}";
+        //String patterJson ="{'name': 'Virtus', 'difficulty': 4, 'pattern': [{'constraintValue': 2 ,'constraintColor': 'Blue' },{'constraintValue': 1,'constraintColor':'Green'}]}";
+        try {
+            BufferedReader reader = new BufferedReader((new FileReader("C:\\Users\\Dario Miceli\\IdeaProjects\\ing-sw-2018-fadda-miceli-mundo\\src\\main\\java\\it\\polimi\\se2018\\json\\Virtus.json")));
+            //System.out.println(reader.lines().collect(Collectors.joining()));
+            Gson gson = new Gson();
+            String string = reader.lines().collect(Collectors.joining());
 
-        Gson gson = new Gson();
+            PatternCard patternCardObject = gson.fromJson(string, PatternCard.class);
 
-
-        PatternCard patternCardObject = gson.fromJson(patterJson, PatternCard.class);
-
+        }
+        catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+        }
     }
-}
+
