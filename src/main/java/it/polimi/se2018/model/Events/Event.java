78678cbaaefdb7ab57cf0b9010a890ca762fbe98
adrianomundo <adriano.mundo@mail.polimi.it>
2018-05-22 20:10:31 +0200

    not committed observable pattern in last commit

---
+++ src/main/java/it/polimi/se2018/model/Events/Event.java
@@ -0,0 +1,10 @@
+package it.polimi.se2018.model.Events;
+
+import java.io.Serializable;
+
+public interface Event extends Serializable {
+
+    //TODO event that should be sent between server/client and mvc
+
+
+}
