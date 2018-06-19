e86c0755d2a6d7481b40d2189f9a6521f9c45a51
salvatoremacbook <salvatoref1996@gmail.com>
2018-06-20 01:42:57 +0200

    Little modify

--- src/main/java/it/polimi/se2018/client/view/gui/EndGameScene.java
+++ src/main/java/it/polimi/se2018/client/view/gui/EndGameScene.java
@@ -1,10 +1,17 @@
 package it.polimi.se2018.client.view.gui;
 
 import it.polimi.se2018.server.model.Components.Player;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -21,6 +28,26 @@ public class EndGameScene {
     private static void setPlayerList(List<Player> playerList){
 
         EndGameScene.playerList = playerList;
+    }
+
+
+    public void dispaly() throws IOException {
+        Stage window = new Stage();
+
+        //Block events to other windows
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("GAME OVER");
+        FXMLLoader loader = new FXMLLoader(RoundTrackerBox.class.getResource("/EndGame.fxml"));
+        Parent root1 = (Parent) loader.load();
+        Scene scene = new Scene(root1);
+        window.setScene(scene);
+        window.setResizable(false);
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                window.showAndWait();
+            }
+        });
     }
 
     public void inizialize(){
